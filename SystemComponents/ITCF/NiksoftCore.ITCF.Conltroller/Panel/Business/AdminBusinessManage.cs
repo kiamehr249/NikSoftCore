e62b9694d9d41e112cab320ba92a046abdfc09e4
kiamehr <kiamehr.hosseini@gmail.com>
2021-03-06 16:54:25 +0330

    Ar and En to products

--- SystemComponents/ITCF/NiksoftCore.ITCF.Conltroller/Panel/Business/AdminBusinessManage.cs
+++ SystemComponents/ITCF/NiksoftCore.ITCF.Conltroller/Panel/Business/AdminBusinessManage.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using NiksoftCore.ITCF.Service;
 using NiksoftCore.MiddlController.Middles;
 using NiksoftCore.Utilities;
+using NiksoftCore.ViewModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -305,5 +307,409 @@ namespace NiksoftCore.ITCF.Conltroller.Panel.Business
 
             return result;
         }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Products([FromQuery] string lang, int part, int bid)
+        {
+            //var theUser = await userManager.GetUserAsync(HttpContext.User);
+            var theBusiness = await iITCFServ.IBusinessServ.FindAsync(x => x.Id == bid);
+            if (theBusiness == null)
+            {
+                return Redirect("/Panel");
+            }
+
+            if (!string.IsNullOrEmpty(lang))
+                lang = lang.ToLower();
+            else
+                lang = defaultLang.ShortName.ToLower();
+
+            ViewBag.Company = theBusiness;
+
+            if (lang == "fa")
+                ViewBag.PageTitle = "محصولات";
+            else
+                ViewBag.PageTitle = "Company Content Management";
+
+            var total = iITCFServ.iProductServ.Count(x => x.BusinessId == bid);
+            var pager = new Pagination(total, 20, part);
+            ViewBag.Pager = pager;
+
+            ViewBag.Contents = iITCFServ.iProductServ.GetAll(x => x.BusinessId == bid).ToList();
+
+            return View(GetViewName(lang, "Products"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct([FromQuery] string lang, int bid)
+        {
+            //var theUser = await userManager.GetUserAsync(HttpContext.User);
+            var theBusiness = await iITCFServ.IBusinessServ.FindAsync(x => x.Id == bid);
+            if (theBusiness == null)
+            {
+                return Redirect("/Panel");
+            }
+            ViewBag.Company = theBusiness;
+
+            if (!string.IsNullOrEmpty(lang))
+                lang = lang.ToLower();
+            else
+                lang = defaultLang.ShortName.ToLower();
+
+            if (lang == "fa")
+                ViewBag.PageTitle = "ایجاد محصول";
+            else
+                ViewBag.PageTitle = "Create Product";
+
+            var request = new ProductRequest();
+            request.BusinessId = bid;
+            request.BusinessCatId = theBusiness.CatgoryId;
+            DropDownBinder(request);
+            return View(GetViewName(lang, "CreateProduct"), request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromQuery] string lang, ProductRequest request)
+        {
+            //var theUser = await userManager.GetUserAsync(HttpContext.User);
+            var theBusiness = await iITCFServ.IBusinessServ.FindAsync(x => x.Id == request.BusinessId);
+            if (theBusiness == null)
+            {
+                return Redirect("/Panel");
+            }
+            ViewBag.Company = theBusiness;
+
+            if (!string.IsNullOrEmpty(lang))
+                lang = lang.ToLower();
+            else
+                lang = defaultLang.ShortName.ToLower();
+
+            if (!FormProduct(lang, request))
+            {
+                ViewBag.Messages = Messages;
+                request.BusinessCatId = theBusiness.CatgoryId;
+                DropDownBinder(request);
+                return View(GetViewName(lang, "CreateProduct"), request);
+            }
+
+            string Image = string.Empty;
+            if (request.ImageFile != null && request.ImageFile.Length > 0)
+            {
+                var SaveImage = await NikTools.SaveFileAsync(new SaveFileRequest
+                {
+                    File = request.ImageFile,
+                    RootPath = hosting.ContentRootPath,
+                    UnitPath = Config.GetSection("FileRoot:BusinessFile").Value
+                });
+
+                if (!SaveImage.Success)
+                {
+                    Messages.Add(new NikMessage
+                    {
+                        Message = "آپلود فایل انجام نشد مجدد تلاش کنید",
+                        Type = MessageType.Error,
+                        Language = "Fa"
+                    });
+                    ViewBag.Messages = Messages;
+                    DropDownBinder(request);
+                    return View(GetViewName(lang, "CreateProduct"), request);
+                }
+
+                Image = SaveImage.FilePath;
+            }
+
+            string Video = string.Empty;
+            if (request.VideoFile != null && request.VideoFile.Length > 0)
+            {
+                var SaveImage = await NikTools.SaveFileAsync(new SaveFileRequest
+                {
+                    File = request.VideoFile,
+                    RootPath = hosting.ContentRootPath,
+                    UnitPath = Config.GetSection("FileRoot:BusinessFile").Value
+                });
+
+                if (!SaveImage.Success)
+                {
+                    Messages.Add(new NikMessage
+                    {
+                        Message = "آپلود فایل انجام نشد مجدد تلاش کنید",
+                        Type = MessageType.Error,
+                        Language = "Fa"
+                    });
+                    ViewBag.Messages = Messages;
+                    DropDownBinder(request);
+                    return View(GetViewName(lang, "CreateProduct"), request);
+                }
+
+                Video = SaveImage.FilePath;
+            }
+
+            var newItem = new Product
+            {
+                Title = request.Title,
+                EnTitle = request.EnTitle,
+                ArTitle = request.ArTitle,
+                Description = request.Description,
+                EnDescription = request.EnDescription,
+                ArDescription = request.ArDescription,
+                Image = Image,
+                Video = Video,
+                CategoryId = request.CategoryId,
+                BusinessId = request.BusinessId
+            };
+
+            iITCFServ.iProductServ.Add(newItem);
+            await iITCFServ.iProductServ.SaveChangesAsync();
+            return Redirect("/Panel/AdminBusinessManage/Products/?bid=" + request.BusinessId);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditProduct([FromQuery] string lang, int Id)
+        {
+            //var theUser = await userManager.GetUserAsync(HttpContext.User);
+            var item = iITCFServ.iProductServ.Find(x => x.Id == Id);
+            var theBusiness = await iITCFServ.IBusinessServ.FindAsync(x => x.Id == item.BusinessId);
+            if (theBusiness == null)
+            {
+                return Redirect("/Panel");
+            }
+            ViewBag.Company = theBusiness;
+
+            if (!string.IsNullOrEmpty(lang))
+                lang = lang.ToLower();
+            else
+                lang = defaultLang.ShortName.ToLower();
+
+            if (lang == "fa")
+                ViewBag.PageTitle = "بروزرسانی محصول";
+            else
+                ViewBag.PageTitle = "Update Product";
+
+            var request = new ProductRequest();
+            request.Title = item.Title;
+            request.EnTitle = item.EnTitle;
+            request.ArTitle = item.ArTitle;
+            request.Description = item.Description;
+            request.EnDescription = item.EnDescription;
+            request.ArDescription = item.ArDescription;
+            request.Image = item.Image;
+            request.Video = item.Video;
+            request.CategoryId = item.CategoryId;
+            request.BusinessId = item.BusinessId;
+            request.BusinessCatId = theBusiness.CatgoryId;
+            DropDownBinder(request);
+            return View(GetViewName(lang, "EditProduct"), request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProduct([FromQuery] string lang, ProductRequest request)
+        {
+            //var theUser = await userManager.GetUserAsync(HttpContext.User);
+            var item = await iITCFServ.iProductServ.FindAsync(x => x.Id == request.Id);
+            var theBusiness = await iITCFServ.IBusinessServ.FindAsync(x => x.Id == request.BusinessId);
+            request.BusinessCatId = theBusiness.CatgoryId;
+            if (theBusiness == null)
+            {
+                return Redirect("/Panel");
+            }
+            ViewBag.Company = theBusiness;
+
+            if (!string.IsNullOrEmpty(lang))
+                lang = lang.ToLower();
+            else
+                lang = defaultLang.ShortName.ToLower();
+
+            if (!FormProduct(lang, request))
+            {
+                ViewBag.Messages = Messages;
+                DropDownBinder(request);
+                return View(GetViewName(lang, "EditProduct"), request);
+            }
+
+            string Image = string.Empty;
+            if (request.ImageFile != null && request.ImageFile.Length > 0)
+            {
+                var SaveImage = await NikTools.SaveFileAsync(new SaveFileRequest
+                {
+                    File = request.ImageFile,
+                    RootPath = hosting.ContentRootPath,
+                    UnitPath = Config.GetSection("FileRoot:BusinessFile").Value
+                });
+
+                if (!SaveImage.Success)
+                {
+                    Messages.Add(new NikMessage
+                    {
+                        Message = "آپلود فایل انجام نشد مجدد تلاش کنید",
+                        Type = MessageType.Error,
+                        Language = "Fa"
+                    });
+                    ViewBag.Messages = Messages;
+                    DropDownBinder(request);
+                    return View(GetViewName(lang, "CreateGroup"), request);
+                }
+
+                Image = SaveImage.FilePath;
+            }
+
+            string Video = string.Empty;
+            if (request.VideoFile != null && request.VideoFile.Length > 0)
+            {
+                var SaveImage = await NikTools.SaveFileAsync(new SaveFileRequest
+                {
+                    File = request.VideoFile,
+                    RootPath = hosting.ContentRootPath,
+                    UnitPath = Config.GetSection("FileRoot:BusinessFile").Value
+                });
+
+                if (!SaveImage.Success)
+                {
+                    Messages.Add(new NikMessage
+                    {
+                        Message = "آپلود فایل انجام نشد مجدد تلاش کنید",
+                        Type = MessageType.Error,
+                        Language = "Fa"
+                    });
+                    ViewBag.Messages = Messages;
+                    DropDownBinder(request);
+                    return View(GetViewName(lang, "EditProduct"), request);
+                }
+
+                Video = SaveImage.FilePath;
+            }
+
+            item.CategoryId = request.CategoryId;
+            item.Title = request.Title;
+            item.EnTitle = request.EnTitle;
+            item.ArTitle = request.ArTitle;
+            item.Description = request.Description;
+            item.EnDescription = request.EnDescription;
+            item.ArDescription = request.ArDescription;
+            if (!string.IsNullOrEmpty(Image))
+                item.Image = Image;
+            if (!string.IsNullOrEmpty(Video))
+                item.Video = Video;
+            await iITCFServ.iProductServ.SaveChangesAsync();
+            return Redirect("/Panel/AdminBusinessManage/Products/?bid=" + request.BusinessId);
+        }
+
+        public async Task<IActionResult> RemoveProduct(int Id)
+        {
+            var theContent = iITCFServ.iProductServ.Find(x => x.Id == Id);
+            int bid = theContent.BusinessId;
+            if (!string.IsNullOrEmpty(theContent.Image))
+            {
+                NikTools.RemoveFile(new RemoveFileRequest
+                {
+                    RootPath = hosting.ContentRootPath,
+                    FilePath = theContent.Image
+                });
+            }
+
+            if (!string.IsNullOrEmpty(theContent.Video))
+            {
+                NikTools.RemoveFile(new RemoveFileRequest
+                {
+                    RootPath = hosting.ContentRootPath,
+                    FilePath = theContent.Video
+                });
+            }
+
+            iITCFServ.iProductServ.Remove(theContent);
+            await iITCFServ.iProductServ.SaveChangesAsync();
+            return Redirect("/Panel/AdminBusinessManage/Products/?bid=" + bid);
+        }
+
+        private void DropDownBinder(ProductRequest request)
+        {
+            var cats = new List<BusinessCategory>();
+            var categories = iITCFServ.IBusinessCategoryServ.GetAll(x => x.ParentId == request.BusinessCatId);
+            foreach (var item in categories)
+            {
+                cats.Add(item);
+                foreach (var subs in item.Childs)
+                {
+                    cats.Add(item);
+                }
+            }
+
+            ViewBag.Categories = new SelectList(cats, "Id", "Title", request?.CategoryId);
+        }
+
+
+
+        private bool FormProduct(string lang, ProductRequest request)
+        {
+            bool result = true;
+            if (string.IsNullOrEmpty(request.Title))
+            {
+                if (lang == "fa")
+                    AddError("عنوان باید مقدار داشته باشد", "fa");
+                else
+                    AddError("Title can not be null", "en");
+                result = false;
+            }
+
+            if (string.IsNullOrEmpty(request.Description))
+            {
+                if (lang == "fa")
+                    AddError("توضیحات باید مقدار داشته باشد", "fa");
+                else
+                    AddError("Description can not be null", "en");
+                result = false;
+            }
+
+            if (request.CategoryId == 0)
+            {
+                if (lang == "fa")
+                    AddError("دسته بندی باید مقدار داشته باشد", "fa");
+                else
+                    AddError("Description can not be null", "en");
+                result = false;
+            }
+
+            if (request.Id == 0 && request.ImageFile == null)
+            {
+                if (lang == "fa")
+                    AddError("تصویر نمی تواند خالی باشد", "fa");
+                else
+                    AddError("Title can not be null", "en");
+                result = false;
+            }
+
+            if (request.ImageFile != null && request.ImageFile.Length > 512000)
+            {
+                if (lang == "fa")
+                    AddError("حجم تصویر نباید بیشتر از 500 KB باشد", "fa");
+                else
+                    AddError("Title can not be null", "en");
+                result = false;
+            }
+
+            if (request.VideoFile != null && request.VideoFile.FileName.GetExtention() != "mp4")
+            {
+                if (lang == "fa")
+                    AddError("فرمت فایل صحیح نیست", "fa");
+                else
+                    AddError("Title can not be null", "en");
+                result = false;
+            }
+
+            if (request.VideoFile != null && request.VideoFile.Length > 2242880)
+            {
+                if (lang == "fa")
+                    AddError("حجم ویدیو نباید بیشتر از 5 MB باشد", "fa");
+                else
+                    AddError("Title can not be null", "en");
+                result = false;
+            }
+
+            return result;
+        }
+
+
+
+
     }
 }
--- SystemComponents/ITCF/NiksoftCore.ITCF.Service/Model/Product.cs
+++ SystemComponents/ITCF/NiksoftCore.ITCF.Service/Model/Product.cs
@@ -6,7 +6,11 @@ namespace NiksoftCore.ITCF.Service
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string EnTitle { get; set; }
+        public string ArTitle { get; set; }
         public string Description { get; set; }
+        public string EnDescription { get; set; }
+        public string ArDescription { get; set; }
         public string Image { get; set; }
         public string Video { get; set; }
         public int CategoryId { get; set; }
