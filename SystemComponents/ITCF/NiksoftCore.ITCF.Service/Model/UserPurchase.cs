946e093b286e71b30ad6913cd640b22239b8c356
kiamehr <kiamehr.hosseini@gmail.com>
2021-03-17 00:59:07 +0330

    model changes

--- SystemComponents/ITCF/NiksoftCore.ITCF.Service/Model/UserPurchase.cs
+++ SystemComponents/ITCF/NiksoftCore.ITCF.Service/Model/UserPurchase.cs
@@ -8,9 +8,13 @@ namespace NiksoftCore.ITCF.Service
         public int UserId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
+        public long PrePayment { get; set; }
+        public long UnitAmount { get; set; }
+        public long TransportAmount { get; set; }
         public DeliveryType DeliveryType { get; set; }
         public PurchaseStatus Status { get; set; }
         public DateTime CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
 
         public virtual Product Product { get; set; }
         public virtual UserModel User { get; set; } 
