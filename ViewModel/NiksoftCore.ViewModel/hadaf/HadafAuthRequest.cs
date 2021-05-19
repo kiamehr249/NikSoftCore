e18d2388ca19e47a32118aa11980b7f42c8070e1
kiamehr <kiamehr.hosseini@gmail.com>
2021-05-20 02:48:32 +0430

    external Reference

---
+++ ViewModel/NiksoftCore.ViewModel/hadaf/HadafAuthRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace NiksoftCore.ViewModel
+{
+    public class HadafAuthRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Command { get; set; }
+    }
+}
